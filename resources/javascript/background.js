f4da867e0899a5e0b9c49811ee342d44c7d8e758
엄두성 <enterkey88@gmail.com>
2014-07-28 19:47:43 +0900

    안쓰는 소스일듯 ..

---
+++ resources/javascript/background.js
@@ -0,0 +1,22 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Returns a handler which will open a new window when activated.
+ */
+function getClickHandler(info, tab) {
+	console.log("info: " + JSON.stringify(info));
+	console.log("tab: " + JSON.stringify(tab));
+	console.dir(info);
+	console.dir(tab);
+};
+
+/**
+ * Create a context menu which will only show up for images.
+ */
+chrome.contextMenus.create({
+	"title" : "My Extension에 저장",
+	"type" : "normal",
+	"onclick" : getClickHandler()
+});
--- resources/javascript/visitedUrls.js
+++ resources/javascript/visitedUrls.js
@@ -13,74 +13,62 @@ visitPage.buildVisitedSiteList = function () {
 			code 			= null,
 			visitedPageInfo = JSON.parse(window.localStorage.getItem(url));
 			
-		if (visitedPageInfo == null) {
-			visitedPageInfo				= {};
-			visitedPageInfo.url 		= url;
-			visitedPageInfo.title 		= title;
-			visitedPageInfo.date 		= new Date();
-			visitedPageInfo.visitedCnt	= 1;
-		} else {
+		
+		if (visitedPageInfo == null) { // 기존에 방문 했던 페이지 인지 조사
+			if(url.indexOf("https://www.facebook.com") != -1) { // 처음 방문한 페이지 인 경우
+				// facebook 인 경우, facebook은 userContent
+				if((url.indexOf('/posts/') != -1 || url.indexOf('/permalink/') != -1)) {
+					code = 'var userContent = document.getElementsByClassName("userContent");' +
+						   'userContent = userContent[0].innerHTML' + 
+				           '({' +
+				           '    title: document.title,' +
+				           '    description: userContent || ""' +
+				           '});';
+				}
+			} else { // facebook 이 아닌 경우	
+				var meta = $('meta[name=description]').attr('content');
+				code = 'var meta = document.querySelector("meta[name=\'description\']");' + 
+			           'if (meta) meta = meta.getAttribute("content");' +
+			           '({' +
+			           '    title: document.title,' +
+			           '    description: meta || ""' +
+			           '});';
+			}
+			
+			chrome.tabs.executeScript({
+			    code: code
+			}, function(results) {
+			    if (!results) {
+			        // An error occurred at executing the script. You've probably not got the permission to execute a content script for the current tab
+			        return;
+			    }
+				console.dir(results);			    
+			    var result 			= {};
+			    result.title 		= results[0].title;
+			    result.description 	= results[0].description;
+			    // Now, do something with result.title and result.description
+				
+			    visitedPageInfo				= {};
+				visitedPageInfo.url 		= url;
+				visitedPageInfo.title 		= title;
+				visitedPageInfo.date 		= new Date();
+				visitedPageInfo.description = result.description;
+				visitedPageInfo.visitedCnt	= 1;
+				
+				// 중복되는 부분이므로 함수로 따로 빼주자
+				window.localStorage.setItem(visitedPageInfo.url, JSON.stringify(visitedPageInfo));
+				visitPage.buildPopupDom();
+			});
+			
+		} else {	// 처음 방문한 페이지가 아닌 경우
+			
 			visitedPageInfo.visitedCnt += 1;
+			window.localStorage.setItem(visitedPageInfo.url, JSON.stringify(visitedPageInfo));
+			visitPage.buildPopupDom();
 		}
 		
-		var visitPageType = visitPage.getVisitPageType();
-		
-		if((url.indexOf("https://www.facebook.com") != -1) && (url.indexOf('/posts/') != -1 || url.indexOf('/permalink/') != -1)) {
-			// facebook 인 경우, facebook은 userContent
-			code = 'var userContent = document.getElementsByClassName("userContent");' +
-				   'userContent = userContent[0].innerHTML' + 
-		           '({' +
-		           '    title: document.title,' +
-		           '    description: userContent || ""' +
-		           '});';
-		} else {
-			// facebook 이 아닌 경우	
-			// var meta = $('meta[name=description]').attr('content');
-			code = 'var meta = document.querySelector("meta[name=\'description\']");' + 
-		           'if (meta) meta = meta.getAttribute("content");' +
-		           '({' +
-		           '    title: document.title,' +
-		           '    description: meta || ""' +
-		           '});';
-		}
-		
-		chrome.tabs.executeScript({
-		    code: code
-		}, function(results) {
-		    if (!results) {
-		        // An error occurred at executing the script. You've probably not got the permission to execute a content script for the current tab
-		        return;
-		    }
-		    var result 			= {};
-		    result.title 		= results[0].title;
-		    result.description 	= results[0].description;
-		    // Now, do something with result.title and result.description
-		    
-		    console.log(result);
-		    console.dir(results);
-			console.log('title : ' + result.title);
-			console.log('description : ' + result.description);			    
-		});
-		
-		
-		window.localStorage.setItem(visitedPageInfo.url, JSON.stringify(visitedPageInfo));
-		visitPage.buildPopupDom();
 	});
 	
-};
-
-visitPage.getVisitPageInfo = function() {
-	
-};
-
-visitPage.getVisitPageType = function() {
-	if((url.indexOf("https://www.facebook.com") != -1) && (url.indexOf('/posts/') != -1 || url.indexOf('/permalink/') != -1)) {
-		return 'facebook';
-	} else if(url.indexOf("https://www.facebook.com") != -1) {
-		return 'error';
-	} else {
-		return 'etc';
-	}
 };
 
 visitPage.buildPopupDom = function () {
@@ -103,8 +91,8 @@ visitPage.buildPopupDom = function () {
 		popupDiv.append(ul);
 
 		for (var i = 0, ie = visitPageInfoList.length; i < ie; ++i) {
-			var a = $('<a>').attr('href', visitPageInfoList[i].url).text('TITLE : ' + visitPageInfoList[i].title + ', URL : ' + visitPageInfoList[i].url + ', VisitCnt : ' + visitPageInfoList[i].visitedCnt);
-			a.bind('click', this.onAnchorClick);
+			var a = $('<a>').attr('href', visitPageInfoList[i].url).text('TITLE : ' + visitPageInfoList[i].title + ', Description : ' + visitPageInfoList[i].description + ', VisitCnt : ' + visitPageInfoList[i].visitedCnt);
+			a.on('click', this.onAnchorClick);
 
 			var li = $('<li>');
 			li.append(a);
@@ -121,6 +109,6 @@ visitPage.onAnchorClick = function (event) {
 	return false;
 };
 
-$(document).bind('DOMContentLoaded', function() {
+$(document).on('DOMContentLoaded', function() {
 	visitPage.buildVisitedSiteList();
 });
