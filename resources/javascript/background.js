f4bd778765607d593a3ef84e48f054245ffee980
엄두성 <enterkey88@gmail.com>
2014-07-29 15:57:26 +0900

    소스 리팩토링 ( 모든 메소드를 객체에 담음)

--- resources/javascript/background.js
+++ resources/javascript/background.js
@@ -1,10 +1,14 @@
-function getClickHandler(info, tab) {
+if ( typeof (myAppBgService) == typeof (undefined)) {
+	myAppBgService = {};
+}
+
+myAppBgService.getClickHandler = function (info, tab) { 
     chrome.tabs.query({
         "active": true,
         "currentWindow": true
     }, function (tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {
-            "functiontoInvoke": "addVisitInfo"
+            "functiontoInvoke": "saveScrapInfo"
         });
     });
 };
@@ -13,6 +17,6 @@ chrome.contextMenus.create({
 	"title" : "My Extension에 저장",
 	"type" : "normal",
 	"contexts" : ["all"],
-	"onclick" : getClickHandler
+	"onclick" : myAppBgService.getClickHandler
 }, null);
 
---
+++ resources/javascript/main.js
@@ -0,0 +1,121 @@
+if ( typeof (myAppMainService) == typeof (undefined)) {
+	myAppMainService = {};
+}
+
+myAppMainService = {
+	MOUSE_VISITED_CLASSNAME : 'crx_mouse_visited',
+	prevDOM : null,
+	
+	scrapInfo : {
+		url 	: null,
+		title 	: null,
+		content : null 
+	}
+};
+
+myAppMainService.getScrapedTargetInfo = function(url, title, srcElement) {
+	if (url.indexOf("https://www.facebook.com") != -1) {// 처음 방문한 페이지 인 경우
+		// facebook 인 경우, facebook은 userContent
+		if ((url.indexOf('/posts/') != -1 || url.indexOf('/permalink/') != -1)) {
+			var userContent = document.getElementsByClassName("userContent");
+			userContent = userContent[0].innerText;
+
+			console.log(userContent);
+			this.setScrapInfo(url, title, userContent);
+		} else {
+			var userContentWrapper = myAppMainService.findParentClass(srcElement);
+
+			if (userContentWrapper != null) {
+				var userContent 		= userContentWrapper.getElementsByClassName('userContent'), 
+					userContentLink 	= userContentWrapper.getElementsByClassName('_5pcq'), 
+					href 				= userContentLink[0].href, 
+					innerText  			= userContent[0].innerText;
+
+				console.log(href);
+				console.log(innerText);
+				this.setScrapInfo(href, title, innerText);
+			}
+		}
+	} else {
+		// facebook이 아닌 경우
+		console.log(srcElement.innerText);
+		this.setScrapInfo(url, title, srcElement.innerText);
+	}
+};
+
+myAppMainService.findParentClass = function(el) {
+	while (el.parentNode) {
+		el = el.parentNode;
+		if (el.className.indexOf('userContentWrapper') != -1)
+			return el;
+	}
+	return null;
+};
+
+myAppMainService.setScrapInfo = function(url, title, content) {
+	this.scrapInfo.url 		= url;
+	this.scrapInfo.title 	= title;
+	this.scrapInfo.content	 = content;
+};
+
+myAppMainService.highlightSelectedDiv = function(srcElement) {
+	if (srcElement.nodeName == 'DIV') {
+		if (myAppMainService.prevDOM != null) {
+			myAppMainService.prevDOM.classList.remove(myAppMainService.MOUSE_VISITED_CLASSNAME);
+		}
+
+		srcElement.classList.add(myAppMainService.MOUSE_VISITED_CLASSNAME);
+		myAppMainService.prevDOM = srcElement;
+	}
+};
+
+
+document.addEventListener('mousemove', function(e) {
+	var srcElement 	= e.srcElement, 
+		url 		= document.URL,
+		title		= document.title;
+		
+	myAppMainService.getScrapedTargetInfo(url, title, srcElement);
+	myAppMainService.highlightSelectedDiv(srcElement);
+}, false);
+ 
+chrome.extension.onMessage.addListener(function(message, sender, callback) {
+	if (message.functiontoInvoke == "saveScrapInfo") {
+		myAppMainService.showInfo();
+		
+	} else if (message.functiontoInvoke == "loadScrapInfo") {
+		myAppMainService.showAnotherInfo();
+	}
+});
+
+myAppMainService.saveScrapInfo = function() {
+	console.log("saveScrapInfo");
+	
+	// var tab 			= tabs[0],
+		// url 			= tab.url, 
+		// title 			= tab.title,
+		// code 			= null,
+		// visitedPageInfo = JSON.parse(window.localStorage.getItem(url));
+// 			
+// 		
+	// if (visitedPageInfo == null) {
+		// console.dir(results);			    
+	    // var result 			= {};
+	    // result.title 		= results[0].title;
+	    // result.description 	= results[0].description;
+// 		
+	    // visitedPageInfo				= {};
+		// visitedPageInfo.url 		= url;
+		// visitedPageInfo.title 		= title;
+		// visitedPageInfo.date 		= new Date();
+		// visitedPageInfo.description = result.description;
+		// visitedPageInfo.visitedCnt	= 1;
+// 		
+		// window.localStorage.setItem(visitedPageInfo.url, JSON.stringify(visitedPageInfo));
+		// visitPage.buildPopupDom();
+	// }	
+};
+
+myAppMainService.loadScrapInfo = function() {
+	console.log("loadScrapInfo");
+};
