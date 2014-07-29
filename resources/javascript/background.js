ed3187cf028cd621047c89abcc57fe0b4cf86d79
엄두성 <enterkey88@gmail.com>
2014-07-29 14:59:20 +0900

    마우스 우클릭 후 애플리케이션 실행 시 메소드 실행 되도록 기능 추가

--- resources/javascript/background.js
+++ resources/javascript/background.js
@@ -1,20 +1,14 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-/**
- * Returns a handler which will open a new window when activated.
- */
 function getClickHandler(info, tab) {
-	console.log("info: " + JSON.stringify(info));
-	console.log("tab: " + JSON.stringify(tab));
-	console.dir(info);
-	console.dir(tab);
+    chrome.tabs.query({
+        "active": true,
+        "currentWindow": true
+    }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {
+            "functiontoInvoke": "addVisitInfo"
+        });
+    });
 };
 
-/**
- * Create a context menu which will only show up for images.
- */
 chrome.contextMenus.create({
 	"title" : "My Extension에 저장",
 	"type" : "normal",
--- resources/javascript/core.js
+++ resources/javascript/core.js
@@ -1,10 +1,6 @@
-// Unique ID for the className.
 var MOUSE_VISITED_CLASSNAME = 'crx_mouse_visited';
-
-// Previous dom, that we want to track, so we can remove the previous styling.
 var prevDOM = null;
 
-// Mouse listener for any move event on the current document.
 document.addEventListener('mousemove', function(e) {
 	var srcElement 	= e.srcElement,
 		url 		= document.URL;
@@ -33,20 +29,12 @@ document.addEventListener('mousemove', function(e) {
 		console.log(srcElement.innerText);
 	}
 	
-	// Lets check if our underlying element is a DIV.
 	if (srcElement.nodeName == 'DIV') {
-
-		// For NPE checking, we check safely. We need to remove the class name
-		// Since we will be styling the new one after.
 		if (prevDOM != null) {
 			prevDOM.classList.remove(MOUSE_VISITED_CLASSNAME);
 		}
 
-		// Add a visited class name to the element. So we can style it.
 		srcElement.classList.add(MOUSE_VISITED_CLASSNAME);
-
-		// The current element is now the previous. So we can remove the class
-		// during the next iteration.
 		prevDOM = srcElement;
 	}
 }, false);
@@ -59,3 +47,21 @@ function findUpTag(el) {
     }
     return null;
 } 
+
+
+var showInfo = function () {
+    console.log("Show Info is invoked");
+};
+
+var showAnotherInfo = function () {
+    console.log("Show Another Info");
+};
+
+chrome.extension.onMessage.addListener(function (message, sender, callback) {
+    if (message.functiontoInvoke == "addVisitInfo") {
+        showInfo();
+    }
+    if (message.functiontoInvoke == "showAnotherInfo") {
+        showAnotherInfo();
+    }
+});
