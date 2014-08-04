c921e5f5d5f6c6caeb2885121c7b96bee61e0374
엄두성 <enterkey88@gmail.com>
2014-08-05 00:46:00 +0900

    브라우저에 클립 아이콘 보이도록 추가, 아이콘 추가, 스크랩 성공 시 메시지 박스 뜨도록 기능 추가

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -20,7 +20,7 @@ myAppMainService.getScrapedTargetInfo = function(url, title, srcElement) {
 			var userContent = document.getElementsByClassName("userContent");
 			userContent = userContent[0].innerText;
 
-			console.log(userContent);
+			// console.log(userContent);
 			this.setScrapInfo(url, title, userContent);
 		} else {
 			var userContentWrapper = myAppMainService.findParentClass(srcElement);
@@ -31,14 +31,14 @@ myAppMainService.getScrapedTargetInfo = function(url, title, srcElement) {
 					href 				= userContentLink[0].href, 
 					innerText  			= userContent[0].innerText;
 
-				console.log(href);
-				console.log(innerText);
+				// console.log(href);
+				// console.log(innerText);
 				this.setScrapInfo(href, title, innerText);
 			}
 		}
 	} else {
 		// facebook이 아닌 경우
-		console.log(srcElement.innerText);
+		// console.log(srcElement.innerText);
 		this.setScrapInfo(url, title, srcElement.innerText);
 	}
 };
@@ -76,7 +76,7 @@ document.addEventListener('mousemove', function(e) {
 		title		= document.title;
 		
 	myAppMainService.getScrapedTargetInfo(url, title, srcElement);
-	myAppMainService.highlightSelectedDiv(srcElement);
+	// myAppMainService.highlightSelectedDiv(srcElement);
 }, false);
  
 chrome.extension.onMessage.addListener(function(message, sender, callback) {
@@ -106,9 +106,32 @@ myAppMainService.saveScrapInfo = function() {
 		scrapInfoSaveRequestData.pageInfo.url 		= myAppMainService.scrapInfo.url;
 		scrapInfoSaveRequestData.pageInfo.content 	= myAppMainService.scrapInfo.content;
 		
-		$.post(scrapInfoSaveRequestURL, scrapInfoSaveRequestData, function(result){
+		 $.post(scrapInfoSaveRequestURL, scrapInfoSaveRequestData, function(result){
 			if (result.status) {
-				console.log('save success');		
+				console.log('save success');	
+				// buildPopupDom();
+				var bubbleDOM = document.createElement('div');
+				bubbleDOM.setAttribute('class', 'selection_bubble');
+				document.body.appendChild(bubbleDOM);
+				
+				var clientWidth = document.body.clientWidth * 80 / 100;
+				var clientHeight = document.body.clientHeight * 2 / 100;
+				
+				var temp = document.body.clientWidth - clientWidth;
+				temp = 200 - temp > 0 ? 210 - temp : 0;
+				clientWidth -= temp;
+				
+				bubbleDOM.innerHTML = 'selection';
+			    bubbleDOM.style.top = clientHeight + 'px';
+			    bubbleDOM.style.left = clientWidth + 'px';
+			    bubbleDOM.style.zIndex = 1000;
+			    bubbleDOM.style.visibility = 'visible';
+			    
+			    setTimeout(function() {
+			    	// bubbleDOM.style.visibility = 'hidden';
+			    	// bubbleDOM.style.zIndex = -1;
+			    	document.body.removeChild(bubbleDOM);
+			    }, 3000);
 			} else {
 				console.log('save error');
 			}
@@ -117,5 +140,5 @@ myAppMainService.saveScrapInfo = function() {
 };
 
 myAppMainService.loadScrapInfo = function() {
-	console.log("loadScrapInfo");
+	// console.log("loadScrapInfo");
 };
