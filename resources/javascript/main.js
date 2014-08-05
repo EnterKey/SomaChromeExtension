9fe05256ec5fa7235ce32458ff5b41024b523497
엄두성 <enterkey88@gmail.com>
2014-08-05 12:55:41 +0900

    scrap 자료 저장 시 메시지 출력 하도록 기능 변경 , 모듈화, jquery 적용

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -20,7 +20,6 @@ myAppMainService.getScrapedTargetInfo = function(url, title, srcElement) {
 			var userContent = document.getElementsByClassName("userContent");
 			userContent = userContent[0].innerText;
 
-			// console.log(userContent);
 			this.setScrapInfo(url, title, userContent);
 		} else {
 			var userContentWrapper = myAppMainService.findParentClass(srcElement);
@@ -31,14 +30,11 @@ myAppMainService.getScrapedTargetInfo = function(url, title, srcElement) {
 					href 				= userContentLink[0].href, 
 					innerText  			= userContent[0].innerText;
 
-				// console.log(href);
-				// console.log(innerText);
 				this.setScrapInfo(href, title, innerText);
 			}
 		}
 	} else {
 		// facebook이 아닌 경우
-		// console.log(srcElement.innerText);
 		this.setScrapInfo(url, title, srcElement.innerText);
 	}
 };
@@ -76,7 +72,6 @@ document.addEventListener('mousemove', function(e) {
 		title		= document.title;
 		
 	myAppMainService.getScrapedTargetInfo(url, title, srcElement);
-	// myAppMainService.highlightSelectedDiv(srcElement);
 }, false);
  
 chrome.extension.onMessage.addListener(function(message, sender, callback) {
@@ -99,6 +94,43 @@ myAppMainService.saveScrapInfo = function() {
 				content : null
 			}
 		};
+		
+	this.scrapInfoSaveRequestResult = {
+		msg : null
+	};
+	this.scrapInfoSaveRequestResult.success = function() {
+		this.msg = '페이지 정보를 저장했습니다.';
+		this.showMessage(this.msg);
+	};
+	this.scrapInfoSaveRequestResult.fail = function() {
+		this.msg = '페이지 정보 저장에 실패했습니다.';
+		this.showMessage(this.msg);
+	};
+	this.scrapInfoSaveRequestResult.showMessage = function(msg) {
+		var bubbleDOM = $('<div>');
+		bubbleDOM.addClass('selection_bubble');
+		$('body').append(bubbleDOM);
+		
+		var bubbleDOMXPosition = document.body.clientWidth * 80 / 100; // 사용자가 보고있는 브라우저 창의 가로
+		var bubbleDOMYPosition = document.body.clientHeight * 2 / 100; // 사용자가 보고있는 브라우저 창의 세로
+		
+		var temp = document.body.clientWidth - bubbleDOMXPosition;
+		temp = 200 - temp > 0 ? 210 - temp : 0;
+		bubbleDOMXPosition -= temp;
+		
+		bubbleDOM.html(msg);
+	    bubbleDOM.css('top', bubbleDOMYPosition + 'px');
+	    bubbleDOM.css('left', bubbleDOMXPosition + 'px');
+	    bubbleDOM.css('zIndex', 1000);
+	    bubbleDOM.css('visibility', 'visible');
+	    
+	    
+	    setTimeout(function() {
+	    	bubbleDOM.fadeOut('slow');
+	    	$('body').remove('.selection_bubble');
+	    }, 3000);
+	};
+	
 	
 	if(url != null) {
 		scrapInfoSaveRequestData.userKey 			= myAppMainService.userKey;
@@ -108,32 +140,11 @@ myAppMainService.saveScrapInfo = function() {
 		
 		 $.post(scrapInfoSaveRequestURL, scrapInfoSaveRequestData, function(result){
 			if (result.status) {
-				console.log('save success');	
-				// buildPopupDom();
-				var bubbleDOM = document.createElement('div');
-				bubbleDOM.setAttribute('class', 'selection_bubble');
-				document.body.appendChild(bubbleDOM);
-				
-				var clientWidth = document.body.clientWidth * 80 / 100;
-				var clientHeight = document.body.clientHeight * 2 / 100;
-				
-				var temp = document.body.clientWidth - clientWidth;
-				temp = 200 - temp > 0 ? 210 - temp : 0;
-				clientWidth -= temp;
-				
-				bubbleDOM.innerHTML = 'selection';
-			    bubbleDOM.style.top = clientHeight + 'px';
-			    bubbleDOM.style.left = clientWidth + 'px';
-			    bubbleDOM.style.zIndex = 1000;
-			    bubbleDOM.style.visibility = 'visible';
-			    
-			    setTimeout(function() {
-			    	// bubbleDOM.style.visibility = 'hidden';
-			    	// bubbleDOM.style.zIndex = -1;
-			    	document.body.removeChild(bubbleDOM);
-			    }, 3000);
+				console.log('save success');
+				myAppMainService.scrapInfoSaveRequestResult.success();
 			} else {
 				console.log('save error');
+				myAppMainService.scrapInfoSaveRequestResult.fail();
 			}
 		});
 	}
@@ -142,3 +153,4 @@ myAppMainService.saveScrapInfo = function() {
 myAppMainService.loadScrapInfo = function() {
 	// console.log("loadScrapInfo");
 };
+
